45b95d19966b3b2b5164badba9319b072f6c2404
SHIVAM KOSHTA <koshta.s974@gmail.com>
2023-07-07 02:59:06 +0530

    Resolving deployment errors

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,12 +4,12 @@ import {getAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB81QOXSbeWGzP_nOJt3LBRPEP5bTJmBmw",
-  authDomain: "leetcode-36d69.firebaseapp.com",
-  projectId: "leetcode-36d69",
-  storageBucket: "leetcode-36d69.appspot.com",
-  messagingSenderId: "592633311667",
-  appId: "1:592633311667:web:086b3bf77532ce903bdbc6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APE_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID 
 };
 
 
